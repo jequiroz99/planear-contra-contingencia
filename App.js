4920e8d7ee6f56c243efc4dd415e2796790f3e7e
jequiroz99 <54960509+jequiroz99@users.noreply.github.com>
2022-11-02 14:03:44 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import MyStack from './navigation/index.js';
+import Home from './screens/home';
+import Quiz from './screens/quiz';
+import Result from './screens/result';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 40,
+    paddingHorizontal: 16,
+  },
+});
+
